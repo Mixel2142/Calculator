901e185de46417c8e5e64ff4313632caa8961323
Mixel2142 <mihefremov@sfedu.ru>
2019-01-26 13:13:16 +0300

    Problems

--- main.cpp
+++ main.cpp
@@ -1,32 +1,54 @@
-#define F_CPU 16000000L
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-
-void key_disp_timer_init()
+int keyDelay = 30;   //кол-во итераций в ISR которое нужно для срабатывания нажатия клавиши
+int keyCounter = 0;  //счётчик кол-ва нажатия в ISR
+ 
+ 
+ bool isKeyPres()
 {
-	TCCR0A = (1 << WGM01);
-	TCCR0B |= (1 << CS02)|(1 << CS00);// Установил делитель на 1024 // 16 MHz / 1024 = 15'625Hz
-	OCR0A = 249;
-	TIMSK0 = (1 << OCIE0A); // Включил работу с TCCR0B
-	sei();
-}
+return PINB > 0b00001111;
+} 
+
 
 ISR(TIMER0_COMPA_vect)
 {
-	PORTB = 0b01010101;
-	PORTD =	0b11110000;	
+	PORTB = PORTB >> 1;
+	if(PORTB <= 0b00000000) PORTB = 0b00001000;
+
+	unsigned char prk = PINB;
+	
+	if(isKeyPres())
+	{
+		keyCounter++;
+	}
+	else 
+	{
+		keyCounter = 0;
+	}//endif
+	
+	if(keyCounter > keyDelay)
+	{
+		PORTD = ~(PINB | 0b00001111);// приравниваю у  порту Д, чтобы визуально было видно,что работает
+		/*
+		Примерно должно выглядеть, как 0b00100010.
+		А получается, что 1 бегает от  0b00001000 до 0b00000001
+
+		Если заменить isKeyPres() на true, то на порт Д выводится всё как должно, но оно должно выводиться во после нажатия кнопки.
+		*/
+		
+		//pressKey = ~(PINB | 0b00001111);	
+		keyCounter = 0;
+	}
+
 }
 
 int main(void)
 {
-	
 	DDRD  = 0b11111111;
 	PORTD = 0b10101010;
 	DDRB  = 0b00001111;
 	PORTB = 0b00000001;
-
 	void key_disp_timer_init();
-	
 	while(1);
 }
